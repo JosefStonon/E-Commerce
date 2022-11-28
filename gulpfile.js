a3571f1db2c60ba2b6bdf9ccb5281d3a9065c4db
JosefStonon <josef.sartori@outlook.com>
2022-11-28 08:18:31 -0300

    criação da pasta dis

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,42 @@
-// para instalar as dependencias: npm install concat --save-dev. O resto segue este padrão.
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const cssmin = require('gulp-cssmin')
+const rename = require('gulp-rename')
+const uglify = require('gulp-uglify')
 
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const cssmin = require('gulp-cssmin');
-const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
-const image = require('gulp-image');
-
-
-function tarefasCSS(cb) {
-
-    return gulp.src('./node_modules/bootstrap/dist/**/*.css')
-        .pipe(concat('libs.css'))
-        .pipe(cssmin())
-        .pipe(rename({ suffix: '.min'}))
-        .pipe(gulp.dest('./dist/css'))
+function tarefaCSS(cb) {
+    return gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css')
+            .pipe(concat('libs.css'))
+            .pipe(cssmin())
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(gulp.dest('./dist/css'))
 }
 
-function tarefasJS() {
-    return gulp.src('./node_modules/bootstrap/dist/**/*.js')
-        .pipe(concat('libs.js'))
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min'}))
-        .pipe(gulp.dest('./dist/js'))
+function tarefasJS(cb) {
+    return gulp.src('./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js')
+            .pipe(concat('libs.js'))
+            .pipe(uglify())
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(gulp.dest('./dist/js'))
 }
 
-function tarefasImagens() {
-    return gulp.src('./src/image/*')
-    .pipe(image({
-      pngquant: true,
-      optipng: false,
-      zopflipng: true,
-      jpegRecompress: false,
-      mozjpeg: true,
-      gifsicle: true,
-      svgo: true,
-      concurrent: 10,
-      quiet: true
-    }))
-    .pipe(gulp.dest('./dist/imagens'))
-};
+//gulp nao reconhece esta função devido node atual nao converte-la de COMMOMJS para ES6.
+// function tarefasIMG(){                
+    
+//    return src('./src/image/*')
+//      .pipe(image({
+//            pngquant: true,
+//            optipng: false,
+//            zopflipng: true,
+//            jpegRecompress: false,
+//            mozjpeg: true,
+//            gifsicle: true,
+//            svgo: true,
+//            concurrent: 10,
+//           quiet: true
+//       }))
+//        .pipe(dest('./dist/images'))
+//    }
 
-
-exports.styles = tarefasCSS
+exports.styles = tarefaCSS
 exports.scripts = tarefasJS
-exports.images = tarefasImagens
