d92b20a91b4795b7696c9583739896674e071708
JosefStonon <josef.sartori@outlook.com>
2022-11-29 18:18:11 -0300

    instalação do babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,39 @@
 const gulp = require('gulp')
+const { series, parallel } = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
+const htmlmin = require('gulp-htmlmin')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
 function tarefaCSS(cb) {
-    return gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css')
+         gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css')
             .pipe(concat('libs.css'))
             .pipe(cssmin())
             .pipe(rename({ suffix: '.min' }))
             .pipe(gulp.dest('./dist/css'))
+
+        return cb()
 }
 
 function tarefasJS(cb) {
-    return gulp.src('./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js')
+         gulp.src('./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js')
+            .pipe(babel({
+                comments: false,
+                presets: ['@babel/env']
+            }))
             .pipe(concat('libs.js'))
             .pipe(uglify())
             .pipe(rename({ suffix: '.min' }))
             .pipe(gulp.dest('./dist/js'))
+
+        return cb()
 }
 
-//gulp nao reconhece esta função devido node atual nao converte-la de COMMOMJS para ES6.
+//gulp nao reconhece esta função devido node nao converte-la de COMMOMJS para ES6.
 // function tarefasIMG(){                
     
 //    return src('./src/image/*')
@@ -38,5 +51,31 @@ function tarefasJS(cb) {
 //        .pipe(dest('./dist/images'))
 //    }
 
+function tarefasHTML(cb) {
+
+        gulp.src('./src/**/*.html')
+            .pipe(htmlmin({ collapseWhitespace: true }))
+            .pipe(gulp.dest('./dist'))
+        
+        return cb()
+            
+}
+
+
+//função para atualizar a pasta dist de forma automatica e sincronizada
+gulp.task('serve', function(){
+        browserSync.init({
+            server: {
+                baseDir: "./dist"
+            }
+        })
+        gulp.watch('./src/**/*').on('change', process)
+        gulp.watch('./src/**/*').on('change', reload)
+})
+
+const process = series( tarefasHTML, tarefaCSS, tarefasJS )
+
 exports.styles = tarefaCSS
 exports.scripts = tarefasJS
+
+exports.default = process
